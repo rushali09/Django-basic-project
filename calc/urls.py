b9f6dbb10c93884f49f6e81992a1f096c5848593
rushali09 <rushalisreedhar37@gmail.com>
2020-04-19 18:45:42 +0530

    second commit

--- calc/urls.py
+++ calc/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 
 urlpatterns = [
-    path('',views.home,name='home')
+    path('',views.home,name='home'), path('add',views.add,name='add')   
 ]
 
 
--- calc/views.py
+++ calc/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
-# Create your views here.
+
+# Create your views here. 
 
 def home(request):
-    return HttpResponse('hello world')   
+    return render(request,'home.htm',{'name':'RushaliSreedhar'}) 
+
+
+def add(request):
+    
+    val1= int (request.POST['num1'])
+    val2= int (request.POST['num2'])
+    res = val1 + val2
+    
+    return render(request,'result.htm',{'result':res})
